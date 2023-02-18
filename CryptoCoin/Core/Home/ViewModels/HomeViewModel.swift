a19d1d1d238d29d871df8437749f92135ef14f87
sinanecodation <122370642+sinanecodation@users.noreply.github.com>
2023-02-18 07:54:07 +0300

    api request

--- CryptoCoin/Core/Home/ViewModels/HomeViewModel.swift
+++ CryptoCoin/Core/Home/ViewModels/HomeViewModel.swift
@@ -8,27 +8,27 @@
 import SwiftUI
 
 class HomeViewModel: ObservableObject {
-    init(){
-        fetchCoinData()
-    }
-    func fetchCoinData(){
-        let urlString = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=true&price_change_percentage=24h"
-        
-        guard let url = URL(string: urlString) else {return}
-        
-        URLSession.shared.dataTask(with: url) { data, response, error in
-            if let error = error {
-                print("DEBUG: Error \(error.localizedDescription)")
-                return
-            }
-            if let response = response as? HTTPURLResponse {
-                print("DEBUG: Response Code \(response.statusCode)")
-            }
-           
-            guard let data = data else {return}
-            let dataAsString = String(data: data, encoding: .utf8)
-            print("DEBUG: data \(dataAsString)")
-        }.resume()
-    }
-    
+    @Published var coins = [Coin]()
+     
+     init() {
+         fetchCoins()
+     }
+     
+     func fetchCoins() {
+         if let url = URL(string: "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=true&price_change_percentage=24h") {
+             URLSession.shared.dataTask(with: url) { data, response, error in
+                 if let data = data {
+                     do {
+                         let decodedResponse = try JSONDecoder().decode([Coin].self, from: data)
+                         DispatchQueue.main.async {
+                             self.coins = decodedResponse
+                             print("DEBUG: Coins \(decodedResponse)")
+                         }
+                     } catch let error {
+                         print("Error: \(error)")
+                     }
+                 }
+             }.resume()
+         }
+     }
 }
