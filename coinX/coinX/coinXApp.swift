19330f28284398b53bdd2c7e89af9113960b4643
sinanecodation <122370642+sinanecodation@users.noreply.github.com>
2023-02-18 06:24:18 +0300

    project setup

---
+++ coinX/coinX/coinXApp.swift
@@ -0,0 +1,17 @@
+//
+//  coinXApp.swift
+//  coinX
+//
+//  Created by SINAN TANRIKUT on 18.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct coinXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
